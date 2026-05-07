8ecec219fa964954b7f3548d6b49cce3697cdd03
Erwin Amador <eamador0305@gmail.com>
2026-05-07 16:38:57 -0600

    Add the principios section

--- script.js
+++ script.js
@@ -18,11 +18,32 @@ const images = [
 const principios = document.querySelector('.principios');
 
 images.forEach((image) => {
+
+  const principio_container = document.createElement('div');
+
   const img = document.createElement('img');
+
+  const img_btn = document.createElement('button');
+
+  principio_container.classList.add('principio_container');
   
   img.classList.add('principio');
 
+  img_btn.classList.add('principio_btn');
+
   img.src = image.image;
 
-  principios.appendChild(img);
+  img_btn.textContent = "Ver Más";
+
+  principios.appendChild(principio_container);
+  
+  principio_container.appendChild(img);
+
+  principio_container.appendChild(img_btn)
 });
+
+function showPopup(popupId) {
+
+  const popup = images.find((popup) => popup.id === popupId);
+
+}
