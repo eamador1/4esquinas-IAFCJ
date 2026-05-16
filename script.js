2bc951d5812f600fa012697dcb6069123d84bfa2
Erwin Amador <eamador0305@gmail.com>
2026-05-15 23:28:58 -0600

    fix linter errors JS

--- script.js
+++ script.js
@@ -1,20 +1,20 @@
 const images = [
   {
     id: '1',
-    image:'img/principios/jesucristo.png',
-    feature:'img/principios/feature-jesucristo.jpeg',
+    image: 'img/principios/jesucristo.png',
+    feature: 'img/principios/feature-jesucristo.jpeg',
     mobile: `Creemos que Jesucristo es el único y verdadero Dios, pues en Él se mezclaron en una forma perfecta e incomprensible 
     los atributos Divinos y la naturaleza humana`,
-    desktop:`Jesucristo fue concebido milagrosamente en el vientre de María, por obra y gracia del Espíritu Santo. 
+    desktop: `Jesucristo fue concebido milagrosamente en el vientre de María, por obra y gracia del Espíritu Santo. 
     El mismo Dios del Antiguo Testamento tomó forma humana. Por lo cual creemos que Jesucristo es el único y verdadero 
     Dios, pues en Él se mezclaron en una forma perfecta e incomprensible los atributos Divinos y la naturaleza humana por 
     eso se le llana Hijo de Dios e Hijo del Hombre (Is.7:14; 52:6; 60:1-3; Lc.1.35; Jn.1:14; Ro 9:5; 1Ti 3:16)`,
   },
-    
+
   {
     id: '2',
-    image:'img/principios/bautismo.png',
-    feature:'img/principios/feature-bautismo.jpeg',
+    image: 'img/principios/bautismo.png',
+    feature: 'img/principios/feature-bautismo.jpeg',
     mobile: `El bautismo es un mandato especial y un gran privilegio pues tiene que ver con su salvación "...el que crea y 
     sea bautizado será salvo" (Mr. 16:16)`,
     desktop: `La gran comisión es de predicar el evangelio y bautizar al que creyere, por lo tanto, el bautismo
@@ -25,25 +25,25 @@ const images = [
 
   {
     id: '3',
-    image:'img/principios/santidad.png',
-    feature:'img/principios/feature-santidad.jpeg',
+    image: 'img/principios/santidad.png',
+    feature: 'img/principios/feature-santidad.jpeg',
     mobile: `Creemos que todos los miembros del cuerpo de Cristo  deben ser santos , es decir, apartados del pecado y consagrados 
     al servicio de Dios.`,
-    desktop:`Creemos que todos los miembros del cuerpo de Cristo  deben ser santos , es decir, apartados del pecado y consagrados 
+    desktop: `Creemos que todos los miembros del cuerpo de Cristo  deben ser santos , es decir, apartados del pecado y consagrados 
     al servicio de Dios. Por esta razón deben de abstenerse de toda clase de prácticas, diversiones e inmundicias de la carne y 
     del espíritu (Lv. 19: 2; 2Co. 7: 1; Ef. 5: 26-27; 1Ti.4: 3-4; 2Ti.2: 21; He.12: 14; 1Ped 1: 15: 17)`,
-  }
+  },
 ];
 
 const principios = document.querySelector('.principios');
 
 images.forEach((image) => {
 
-  const principio_container = document.createElement('div');
+  const principioContainer = document.createElement('div');
 
   const img = document.createElement('img');
 
-  const img_btn = document.createElement('button');
+  const imgBtn = document.createElement('button');
 
   const modal = document.querySelector('.modal');
 
@@ -51,55 +51,52 @@ images.forEach((image) => {
 
   const modalText = document.querySelector('.modal-text');
 
-  const closeBtn = document.querySelector('.close-btn')
+  const closeBtn = document.querySelector('.close-btn');
 
-  principio_container.classList.add('principio_container');
-  
+  principioContainer.classList.add('principio_container');
+
   img.classList.add('principio');
 
-  img_btn.classList.add('principio_btn');
+  imgBtn.classList.add('principio_btn');
 
   img.src = image.image;
 
-  img_btn.textContent = "Ver Más";
+  imgBtn.textContent = "Ver Más";
 
-  img_btn.addEventListener('click', () => {
+  imgBtn.addEventListener('click', () => {
 
-  modal.classList.remove('hidden');
-
-  modalImg.src = image.feature;
-
-  if (window.innerWidth <= 768) {
-    modalText.textContent = image.mobile;
-  } else {
-    modalText.textContent = image.desktop;
-  }
-
-});
-
-  principios.appendChild(principio_container);
+    modal.classList.remove('hidden');
   
-  principio_container.appendChild(img);
+    modalImg.src = image.feature;
 
-  principio_container.appendChild(img_btn)
+    if (window.innerWidth <= 768) {
+      modalText.textContent = image.mobile;
+    } else {
+      modalText.textContent = image.desktop;
+    }
+
+  });
+
+  principios.appendChild(principioContainer);
+
+  principioContainer.appendChild(img);
+
+  principioContainer.appendChild(imgBtn);
 
 closeBtn.addEventListener('click', () => {
-  modal.classList.add('hidden');
-
-});
-
-modal.addEventListener('click', (e) => {
-
-  if (e.target === modal) {
     modal.classList.add('hidden');
-  }
 
 });
 
+  modal.addEventListener('click', (e) => {
+
+    if (e.target === modal) {
+      modal.classList.add('hidden');
+    }
 
 });
 
-
+});
 
 let slideIndex = 1;
 
@@ -123,9 +120,9 @@ function currentSlide(n) {
 // MAIN FUNCTION
 function showSlides(n) {
 
-  const slides = document.getElementsByClassName("mySlides");
+  const slides = document.getElementsByClassName('mySlides');
 
-  const dots = document.getElementsByClassName("dot");
+  const dots = document.getElementsByClassName('dot');
 
   if (n > slides.length) {
     slideIndex = 1;
@@ -140,10 +137,11 @@ function showSlides(n) {
   }
 
   for (let i = 0; i < dots.length; i++) {
-    dots[i].classList.remove("active");
+    dots[i].classList.remove('active');
   }
 
-  slides[slideIndex - 1].style.display = "block";
+  slides[slideIndex - 1].style.display = 'block';
 
-  dots[slideIndex - 1].classList.add("active");
+  dots[slideIndex - 1].classList.add('active');
 }
+
